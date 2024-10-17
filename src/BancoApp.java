d2b49ddcb648833363fffffdb9a88a54a0f75caf
Sena <dimatesebas73@gmail.com>
2024-10-17 10:40:18 -0500

    Creacion de BancoApp

---
+++ src/BancoApp.java
@@ -0,0 +1,67 @@
+//Creacion de la clase bancaria
+
+class cuentaBancaria{
+    String titular;
+    double saldo;
+
+    //Constructor de la clase
+
+    public cuentaBancaria(String titular, double saldoInicial ){
+        this.titular = titular;
+        if (saldoInicial < 0){
+            throw new IllegalArgumentException("El Saldo no puede ser menor a 0");
+        }
+        this.saldo = saldoInicial;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    //Clase retirar dinero
+
+    public void retiro(double cantidad) throws Exception{
+        if(cantidad <= 0){
+            throw new IllegalArgumentException("El monto a retirar debe ser mayor a 0");
+        }
+        if(saldo < cantidad){
+            throw new IllegalArgumentException("Saldo Insuficiente");
+        }
+        saldo -= cantidad;
+        System.out.println("Retiro de: " + cantidad + " Exitoso. \n\tSaldo Restante: " + saldo);
+    }
+
+    //Clase deposito de dinero
+
+    public void deposito(double cantidad){
+        if (cantidad <= 0){
+            throw new IllegalArgumentException("El deposito no puede ser negativo");
+        }
+        saldo += cantidad;
+        System.out.println("Deposito de: " + cantidad + " Exitoso. \n\tSaldo actual " + saldo );
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+}
+
+//Funcion BancoApp
+
+public class BancoApp{
+    public static void main(String[]args){
+        try{
+            cuentaBancaria cuenta1 = new cuentaBancaria("Juan", 100000);
+
+            System.out.println("Su saldo actual es de: " + cuenta1.saldo);
+            cuenta1.deposito(200000);
+            cuenta1.retiro(130000);
+
+
+        }catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
+
+
